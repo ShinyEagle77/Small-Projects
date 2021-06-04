f65c40e33f4cc446ace02bdf57f180b17aaa5f2b
ShinyEagle77 <83186959+ShinyEagle77@users.noreply.github.com>
2021-06-04 21:43:20 +0300

    Add files via upload

---
+++ StartEndStepSum/app13.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int sequenceSum(int start, int end, int step)
+{
+
+	int sum = 0;
+	int sumAfter = 0;
+
+	if (start <= end)
+	{
+		for (; start <= end; start += step)
+		{
+			sum += start;
+		}
+
+		return sum;
+		
+	}
+	else
+		return 0;
+}
+
+
+int main()
+{
+	int start, end, step;
+	cin >> start >> end >> step;
+
+	cout << sequenceSum(start, end, step) << endl;
+
+	return 0;
+}
