1ccf280a378bcceca630ebbc95ad48ea4aafca37
ShinyEagle77 <83186959+ShinyEagle77@users.noreply.github.com>
2021-06-04 21:40:52 +0300

    Add files via upload

---
+++ MaxGreatestFactor/app20.cpp
@@ -0,0 +1,37 @@
+﻿/*
+ * В stdin даны два натуральных числа. Выведите в stdout их наибольший общий делитель.
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int getMax(int a, int b)
+{
+    while (a > 0 && b > 0) 
+    {
+
+        if (a > b) 
+        {
+
+            a %= b;
+
+        }
+        else 
+        {
+
+            b %= a;
+        }
+    }
+
+    return a + b;
+}
+
+
+int main()
+{
+	int a, b;
+	cin >> a >> b;
+
+    cout << getMax(a, b) << endl;
+}
